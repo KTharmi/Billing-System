a4f1c4e9cb2d9c9f3ca83241ab79fc60e15fed66
KTharmi <tharmikulendiran5@gmail.com>
2022-04-19 15:33:49 +0530

    admin login panel

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+
+@section('title')
+    Dashboard 
+@endsection
+
+@section('content')
+<table class="table table-sm">
+    <thead>
+        <tr>
+            <th>Statistics</th>
+            <th>#1</th>
+            <th>#2</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Driver Name</td>
+            <td>John Doe</td>
+            <td>Mary Sue</td>
+        </tr>
+        <tr>
+            <td>Origin</td>
+            <td>Downtown</td>
+            <td>Uptown</td>
+        </tr>
+    </tbody>
+</table>
+@endsection
+
+@section('scripts')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth','admin']],function(){
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
